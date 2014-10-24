e27fbe281b05e0599dd49c170d1b38f13f6ab078
Miles Watkins <mileswwatkins@gmail.com>
2014-10-23 23:43:17 -0500

    Added response-getting function

--- amtrak_lateness_predictor.py
+++ amtrak_lateness_predictor.py
@@ -6,61 +6,80 @@ import dateutil
 import requests
 
 
-def parse_response(response_text):
-	'''Extract train time information from the juckins.net response'''
+def get_response(train_number):
+    '''Retrieve the HTML response, or display any errors'''
 
-	# Create a table parser
-	table_parser = BeautifulSoup(response_text)
-	table = table_parser.find("table")
-	rows = iter(table.findAll("tr"))
+    URL = "http://juckins.net/amtrak_status/archive/html/history.php"
+    VERY_LARGE_NUMBER = 1000000
 
-	# Determine the names of each field
-	_table_title = next(rows)
-	field_names = [header.text for header in next(rows)]
+    today = datetime.date.today()
+    last_year = today - datetime.timedelta(days=365)
+    DATE_FORMAT = "%m/%d/%Y"
 
-	# Extract the values from each row
-	all_values = []
-	for row in rows:
-		values = [cell.text for cell in row]
-		values_labeled = dict(itertools.izip(field_names, values))
-		all_values.append(values_labeled)
+    parameters = {
+            "train_num": train_number,
+            "station": "",
+            "date_start": last_year.strftime(DATE_FORMAT),
+            "date_end": today.strftime(DATE_FORMAT),
+            "sort": "schDp",
+            "sort_dir": "DESC",
+            "limit": VERY_LARGE_NUMBER,
+            "co": "gt"
+            }
+    response = requests.get(URL, params=parameters)
 
-	return all_values
+    # Make sure that the response was successful
+    if response.status_code is not 200:
+        response.raise_for_status()
+
+    # Return the responded HTML text
+    return response.text
+
+
+def parse_response_table(response_text):
+    '''Extract train time information from the juckins.net response'''
+
+    # Create a table parser
+    table_parser = BeautifulSoup(response_text)
+    table = table_parser.find("table")
+    rows = iter(table.findAll("tr"))
+
+    # Determine the names of each field
+    _table_title = next(rows)
+    field_names = [header.text for header in next(rows)]
+
+    # Extract the values from each row
+    all_values = []
+    for row in rows:
+        values = [cell.text for cell in row]
+        values_labeled = dict(itertools.izip(field_names, values))
+        all_values.append(values_labeled)
+
+    return all_values
 
 
 def clean_table(table):
-	'''Remove useless columns and convert to proper data types'''
+    '''Remove useless columns and convert text to proper data types'''
 
-	cleaned_table = []
-	for row in table:
-		
-		drop_this_row = False
-		
-		for key in row.keys():
-			if key.startswith("Sch ") or key.startswith("Act "):
-		
-				# Throw out the row if the time data do not exist
-				if row[key].isspace():
-					drop_this_row = True
+    cleaned_table = []
+    for row in table:
+        
+        drop_this_row = False
+        
+        for key in row.keys():
+            if key.startswith("Sch ") or key.startswith("Act "):
+        
+                # Throw out the row if the time data do not exist
+                if row[key].isspace():
+                    drop_this_row = True
 
-				# Convert the plain text values to Python data types
-				row[key] = dateutil.parser.parse(row[key])
-		row["Origin Date"] = dateutil.parser.parse(row["Origin Date"]).date()
+                # Convert the plain text values to Python data types
+                row[key] = dateutil.parser.parse(row[key])
+        row["Origin Date"] = dateutil.parser.parse(row["Origin Date"]).date()
 
-		if drop_this_row == False:
-			cleaned_table.append(row)
+        if drop_this_row == False:
+            cleaned_table.append(row)
 
-	return cleaned_table
-
-
-# Determine current date and comparable weekdays
-today = datetime.date.today()
-ONE_WEEK = datetime.timedelta(days=7)
-WEEKS_IN_A_YEAR = 52
-
-days_to_compare = []
-# for weeks_ago in range(1, WEEKS_IN_A_YEAR + 1):
-# 	day_to_compare = today - ONE_WEEK * weeks_ago
-# 	days_to_compare.append(day_to_compare)
+    return cleaned_table
 
 
