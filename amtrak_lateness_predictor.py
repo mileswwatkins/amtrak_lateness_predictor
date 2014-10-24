1472c0cc15b6e800e4772d8b619101411d181a78
Miles Watkins <mileswwatkins@gmail.com>
2014-10-24 10:31:38 -0500

    Produce very buggy prediction, but at least all the pieces are in place

--- amtrak_lateness_predictor.py
+++ amtrak_lateness_predictor.py
@@ -2,7 +2,8 @@ import datetime
 import itertools
 
 from bs4 import BeautifulSoup
-import dateutil
+# Straight import of <dateutil> and use of parse function does not work
+from dateutil.parser import parse as datetime_parse
 import requests
 
 
@@ -29,14 +30,13 @@ def get_response(train_number):
     response = requests.get(URL, params=parameters)
 
     # Make sure that the response was successful
-    if response.status_code is not 200:
-        response.raise_for_status()
+    response.raise_for_status()
 
-    # Return the responded HTML text
+    # Return the returned HTML text
     return response.text
 
 
-def parse_response_table(response_text):
+def parse_response(response_text):
     '''Extract train time information from the juckins.net response'''
 
     # Create a table parser
@@ -74,8 +74,8 @@ def clean_table(table):
                     drop_this_row = True
 
                 # Convert the plain text values to Python data types
-                row[key] = dateutil.parser.parse(row[key])
-        row["Origin Date"] = dateutil.parser.parse(row["Origin Date"]).date()
+                row[key] = datetime_parse(row[key])
+        row["Origin Date"] = datetime_parse(row["Origin Date"]).date()
 
         if drop_this_row == False:
             cleaned_table.append(row)
@@ -83,3 +83,35 @@ def clean_table(table):
     return cleaned_table
 
 
+def get_prediction_for_train(train_number, destination_station):
+    '''Main function of the module, to perform the prediction'''
+
+    # Get and clean all train data from route for last year
+    response_text = get_response(train_number)
+    raw_table = parse_response(response_text)
+    cleaned_table = clean_table(raw_table)
+
+    # Filter the data for only the given station, and for applicable weekdays
+    cleaned_table = [
+            row for row in cleaned_table if
+            row["Station"] == destination_station and
+            row["Sch DP"].date().weekday() == datetime.date.today().weekday()
+            ]
+
+    # Calculate the delay, based on scheduled and actual times
+    for row in cleaned_table:
+        row["delay"] = row["Sch DP"] - row["Act DP"]
+
+    # Determine the average delay for the given station
+    delays = [row["delay"] for row in cleaned_table]
+    average_delay = sum(delays, datetime.timedelta(0)) / len(delays)
+
+    print(average_delay)
+
+
+# Test example while developing
+if __name__ == "__main__":
+    get_prediction_for_train(
+            train_number=353,
+            destination_station="HMI"
+            )
