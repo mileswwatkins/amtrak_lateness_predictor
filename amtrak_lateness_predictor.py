f7a34733e993f4bfcbf50cc7353dd56a121660ce
Miles Watkins <miles.w.watkins@gmail.com>
2015-12-28 16:15:57 -0600

    Add chart URL for auxilary information

--- amtrak_lateness_predictor.py
+++ amtrak_lateness_predictor.py
@@ -1,42 +1,59 @@
 import datetime
+import logging
 import re
 
 import lxml.html
 import requests
 
 
+# logging.basicConfig(level=logging.DEBUG)
+
+TODAY = datetime.date.today()
+ONE_YEAR_AGO = TODAY - datetime.timedelta(days=365)
+DATE_FORMAT = "%m/%d/%Y"
+VERY_LARGE_NUMBER = 1000000
+
+
+def get_chart_url(train_number, station):
+    ''' Build the URL for a chart that provides auxilary information '''
+
+    parameters = {
+        'train_num': train_number,
+        'station': station,
+        'date_start': ONE_YEAR_AGO.strftime(DATE_FORMAT),
+        'date_end': TODAY.strftime(DATE_FORMAT),
+        'sort': 'd_ar',
+        'chartsize': '2',
+        'smooth': '0'
+    }
+    built_url = requests.Request(
+        url='http://juckins.net/amtrak_status/archive/html/historychart.php',
+        params=parameters
+    ).prepare().url
+    return built_url
+
+
 def get_response(train_number):
     '''Retrieve the HTML response, or display any errors'''
-
-    URL = "http://juckins.net/amtrak_status/archive/html/history.php"
-    VERY_LARGE_NUMBER = 1000000
-
-    TODAY = datetime.date.today()
-    ONE_YEAR_AGO = TODAY - datetime.timedelta(days=365)
-    DB_STARTS_GIVING_ARR_DEP_SEPARATELY = datetime.date(2015, 5, 21)
-    if ONE_YEAR_AGO > DB_STARTS_GIVING_ARR_DEP_SEPARATELY:
-        start_date = ONE_YEAR_AGO
-    else:
-        start_date = DB_STARTS_GIVING_ARR_DEP_SEPARATELY
-    DATE_FORMAT = "%m/%d/%Y"
-
 
     parameters = {
         "train_num": train_number,
         "station": "",
-        "date_start": start_date.strftime(DATE_FORMAT),
+        "date_start": ONE_YEAR_AGO.strftime(DATE_FORMAT),
         "date_end": TODAY.strftime(DATE_FORMAT),
-        "sort": "schAr",
+        "sort": "d_ar",
         "sort_dir": "DESC",
         "limit": VERY_LARGE_NUMBER,
         "co": "gt"
     }
-    response = requests.get(URL, params=parameters)
+    response = requests.get(
+        'http://juckins.net/amtrak_status/archive/html/history.php',
+        params=parameters
+    )
+    logging.debug("URL accessed: {}".format(response.request.url))
 
     # Make sure that the response was successful
     response.raise_for_status()
-
-    # Return the returned HTML text
     return response.text
 
 
@@ -53,68 +70,32 @@ def clean_response(response_text):
     assert len(table[0].xpath('th')) == 1
     assert table[1].xpath('th/text()')[0] == "Origin Date"
     assert table[1].xpath('th/text()')[1] == "Station"
-    assert table[1].xpath('th/text()')[2].startswith("Sch ")
-    assert table[1].xpath('th/text()')[3].startswith("Act ")
+    assert table[1].xpath('th/text()')[2] == "Sch AR"
+    assert table[1].xpath('th/text()')[3] == "Act AR"
     assert table[1].xpath('th/text()')[4] == "Comments"
-    assert table[1].xpath('th/text()')[5] == "Service Disruption"
-    assert table[1].xpath('th/text()')[6] == "Cancellations"
+    assert table[1].xpath('th/text()')[5] == "AR Delay (mins)"
+    assert table[1].xpath('th/text()')[6] == "Service Disruption"
+    assert table[1].xpath('th/text()')[7] == "Cancellations"
     assert len(table[-1].xpath('td')) == 1
 
-    table = table[2:-1]
+    table = table[2:-3]
 
     # Convert to native data types
-    DELAY_RE = (
-        r'^(Arr|Dep):\s+'
-        r'(?:(?P<hours>\d{1,2}) hr(?:,)?)?\s*'
-        r'(?:(?P<minutes>\d{1,2}) min)?\s*'
-        r'(early|late)\.?$'
-    )
-
-    df = []
+    response_table = []
     for row in table:
-        df_row = {}
+        response_table_row = {}
 
-        if 'Average' in row.text_content() or 'Median' in row.text_content():
-            continue
-
-        df_row['station'] = row.xpath('td[2]/text()')[0]
-
-        try:
-            delay = row.xpath('td[5]//text()')[0].strip()
-            # Keep only arrival information if both arrival and departure exist
-            delay = re.sub(r'\s\|\sDep:\s.*', '', delay)
-        except IndexError:
-            print("No delay information found for station {}".format(
-                df_row['station']))
-            continue
-
-        if 'On time' in delay:
-            df_row['delay'] = datetime.timedelta(0)
-        elif re.match(r'^[ECMP][SD]?T$', delay):
-            # Time zone placeholders indicate that there is no actual time data
-            continue
-        elif re.match(DELAY_RE, delay):
-            minutes_off = 0
-            if re.search(DELAY_RE, delay).group("hours"):
-                minutes_off += int(re.search(DELAY_RE, delay).group("hours")) * 60
-            if re.search(DELAY_RE, delay).group("minutes"):
-                minutes_off += int(re.search(DELAY_RE, delay).group("minutes"))
-            if 'early' in delay:
-                minutes_off *= -1
-            df_row['delay'] = datetime.timedelta(seconds=minutes_off * 60)
-        else:
-            print("Unparseable delay information found: '{}'".format(delay))
-            continue
-
-        df_row['scheduled_date'] = datetime.datetime.strptime(
+        response_table_row['station'] = row.xpath('td[2]/text()')[0]
+        response_table_row['delay'] = datetime.timedelta(seconds=int(row.xpath('td[6]//text()')[0]) * 60)
+        response_table_row['scheduled_date'] = datetime.datetime.strptime(
             re.sub(r'\s\([A-Z][a-z]\)', '', row.xpath('td[3]/text()')[0]),
             '%m/%d/%Y %I:%M %p').date()
-        df_row['service_disruption'] = bool(row.xpath('td[6]//text()'))
-        df_row['cancellation'] = bool(row.xpath('td[7]//text()'))
+        response_table_row['service_disruption'] = bool(row.xpath('td[7]//text()'))
+        response_table_row['cancellation'] = bool(row.xpath('td[8]//text()'))
 
-        df.append(df_row)
-
-    return df
+        response_table.append(response_table_row)
+    logging.debug('{} rows in response table'.format(len(response_table)))
+    return response_table
 
 
 def filter_table(full_table, desintation, same_weekday=True):
@@ -134,14 +115,14 @@ def filter_table(full_table, desintation, same_weekday=True):
                 continue
 
         filtered_table.append(row)
-
+    logging.debug('{} rows in filtered table'.format(len(filtered_table)))
     return filtered_table
 
 
 def get_mean_delay(table):
     ''' Calculate the average delay from a table including delays '''
     delays = [row['delay'] for row in table]
-    mean_delay = sum(delays, datetime.timedelta(0)) / len(delays)
+    mean_delay = sum(delays, datetime.timedelta()) / len(delays)
     assert type(mean_delay) == type(datetime.timedelta())
     mean_delay = mean_delay.seconds / 60
     return mean_delay
@@ -150,6 +131,7 @@ def get_mean_delay(table):
 def get_prediction_for_train(train_number, destination):
     '''Main function of the module, to perform the prediction'''
 
+    chart_url = get_chart_url(train_number, destination)
     response_text = get_response(train_number)
     cleaned_table = clean_response(response_text)
     filtered_table = filter_table(cleaned_table, destination)
@@ -157,8 +139,9 @@ def get_prediction_for_train(train_number, destination):
 
     print("Mean delay for train {0} going to station {1}: {2} minutes".format(
         train_number, destination, mean_delay))
+    print("Chart and historical data: {}".format(chart_url))
 
-# Test example while ddelayeveloping
+# Test example while developing
 if __name__ == "__main__":
     get_prediction_for_train(
         train_number=2,
